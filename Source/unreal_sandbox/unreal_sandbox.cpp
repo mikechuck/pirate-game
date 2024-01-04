9f3a74bedafb41782fdb13525f93d2f0c8f687aa
Michael <mcjoy465@gmail.com>
2024-01-04 13:14:15 -0500

    Revert "Fixed inputs, started c++ builds"
    This reverts commit 1f7e954a629e38b81b7114936fd25335391e944f.

--- Source/unreal_sandbox/unreal_sandbox.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "unreal_sandbox.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_sandbox, "unreal_sandbox" );
